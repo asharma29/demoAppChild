d9ef50814e1e4556af1cfb4cbc39c973dd8ee204
Abhishek Sharma <a.banwari07@gmail.com>
2023-04-11 15:58:08 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { ParentsComponent } from './parents/parents.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent,
+    ParentsComponent
   ],
   imports: [
     BrowserModule,
